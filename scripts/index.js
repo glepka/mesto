84eb3a76d681cdb088aaae5bc0e33eb9650b03c0
Gleb Lyashenkov <glepka17@yandex.ru>
2022-07-19 23:22:08 +0300

    Можно добавить свою карточку + 6 самостоятельных.

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,5 @@
 const popupProfile = document.querySelector(".popup_type_profile");
 const popupPlace = document.querySelector(".popup_type_place");
-const popupImage = document.querySelector(".popup_type_image");
 
 const popupCrossProfile = document.querySelector(".popup__cross_type_profile");
 const popupCrossPlace = document.querySelector(".popup__cross_type_place");
@@ -8,7 +7,7 @@ const popupCrossImage = document.querySelector(".popup__cross_type_image");
 
 const editButton = document.querySelector(".profile__edit-button");
 const addButton = document.querySelector(".profile__add-button");
-const imageOfElement = document.querySelector(".elements__image");
+
 const likeButtons = document.querySelectorAll(".elements__icon");
 
 const nameInput = document.querySelector(".form__text_type_name");
@@ -18,6 +17,7 @@ const profileName = document.querySelector(".profile__name");
 const profileProfession = document.querySelector(".profile__subtitle");
 
 const formElement = document.querySelector(".form");
+const formElementPlace = document.querySelector(".form-place");
 
 // ПРОФИЛЬ
 
@@ -65,13 +65,86 @@ function closePopupPlace() {
 }
 popupCrossPlace.addEventListener("click", closePopupPlace);
 
+// КАРТОЧКИ
+const placeOnline = document.querySelector(".elements");
+const placeTemplate = document.querySelector("#place").content;
+const initialCards = [
+  {
+    name: "Архыз",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+  },
+  {
+    name: "Челябинская область",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+  },
+  {
+    name: "Иваново",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+  },
+  {
+    name: "Камчатка",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+  },
+  {
+    name: "Холмогорский район",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+  },
+  {
+    name: "Байкал",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
+];
+
+// Добавить начальные карточки
+
+function addCards(item) {
+  const placeElement = placeTemplate
+    .querySelector(".elements__template")
+    .cloneNode(true);
+
+  placeElement.querySelector(".elements__image").src = item.link;
+  placeElement.querySelector(".elements__title").textContent = item.name;
+
+  placeOnline.append(placeElement);
+}
+initialCards.forEach(addCards);
+// Добавить новую карточку
+
+function formSubmitPlace(evt) {
+  evt.preventDefault();
+  const placeInput = document.querySelector(".form__text_type_place").value;
+  const imageLinkImput = document.querySelector(".form__text_type_link").value;
+  const placeElement = placeTemplate
+    .querySelector(".elements__template")
+    .cloneNode(true);
+
+  initialCards.push({ name: placeInput, link: imageLinkImput });
+  item = initialCards[initialCards.length - 1];
+  placeElement.querySelector(".elements__image").src = item.link;
+  placeElement.querySelector(".elements__title").textContent = item.name;
+
+  placeOnline.prepend(placeElement);
+
+  closePopupPlace();
+}
+
+formElementPlace.addEventListener("submit", formSubmitPlace);
+
 //ПРОСМОТР КАРТИНКИ
 
 //Открыть картинку
+const popupImage = document.querySelector(".popup_type_image");
+const imageOfElement = document.querySelector(".elements__image");
+const popupImageSrc = document.querySelector(".popup__image");
+const popupImageText = document.querySelector(".popup__image-text");
 
 function openPopupImage() {
   popupImage.classList.add("popup_opened");
+
+  popupImageSrc.src = this.src;
+  popupImageText.textContent = placeElement.textContent;
 }
+console.log(imageOfElement);
 imageOfElement.addEventListener("click", openPopupImage);
 
 // Закрыть картинку
@@ -82,7 +155,6 @@ function closePopupImage() {
 popupCrossImage.addEventListener("click", closePopupImage);
 
 // ИКОНКА ЛАЙКА
-
 function toggleLike() {
   this.classList.toggle("elements__icon_type_active");
 }
