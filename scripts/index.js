abecd9b13b67023b16d1cc83920a43b37cd624e4
Gleb Lyashenkov <glepka17@yandex.ru>
2022-07-22 15:59:15 +0300

    Изменена логика в JS файле. Все работает.

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,23 @@
 const page = document.querySelector(".page");
 
 const elements = document.querySelector(".elements");
-const element = document.querySelector(".elements__element");
-//ПОПАПЫ
-const popupProfile = document.querySelector(".popup_type_profile");
-const popupPlace = document.querySelector(".popup_type_place");
-const popupImage = document.querySelector(".popup_type_image");
 
-// КНОПКИ ОТКРЫТИЯ ПОПАПОВ
+const cardTemplate = document.querySelector(".card").content;
+
+//ПОПАПЫ
+const popupTypeProfile = document.querySelector(".popup_type_profile");
+const popupTypePlace = document.querySelector(".popup_type_place");
+const popupTypeImage = document.querySelector(".popup_type_image");
+
+// ДЛЯ ОТКРЫТИЯ ПОПАПОВ
 const editButton = document.querySelector(".profile__edit-button");
 const addPlaceButton = document.querySelector(".profile__add-button");
-const gridImage = document.querySelectorAll(".elements__image");
+//Картинка карточки
+const cardImg = document.querySelectorAll(".elements__image");
 
 //ДАННЫЕ ДЛЯ ПОПАПА КАРТИНКИ
-const popupImageSrc = document.querySelector(".popup__image");
+const popupImage = document.querySelector(".popup__image");
 const popupImageText = document.querySelector(".popup__image-text");
-
-//КНОПКИ ЛАЙКА
-const likeButtons = document.querySelectorAll(".elements__icon");
 
 // ДАННЫЕ ПРОФИЛЯ, КОТОРЫЕ ВВОДИТ ПОЛЬЗОВАТЕЛЬ
 const nameInput = document.querySelector(".form__text_type_name");
@@ -54,13 +54,13 @@ function openPopup(item) {
 // ОТРЫТЬ ПОПАП ПРОФИЛЯ
 
 editButton.addEventListener("click", function () {
-  openPopup(popupProfile);
+  openPopup(popupTypeProfile);
   nameInput.value = profileName.textContent;
   professionInput.value = profileProfession.textContent;
 });
 
 // ОТКРЫТЬ ПОПАП МЕСТА
-addPlaceButton.addEventListener("click", () => openPopup(popupPlace));
+addPlaceButton.addEventListener("click", () => openPopup(popupTypePlace));
 
 // Изменить имя и профессию в профиле
 
@@ -68,13 +68,11 @@ function formSubmitHandler(evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileProfession.textContent = professionInput.value;
-  closePopup(popupProfile);
+  closePopup(popupTypeProfile);
 }
 formElement.addEventListener("submit", formSubmitHandler);
 
 // КАРТОЧКИ
-
-const placeTemplate = document.querySelector("#place").content;
 const initialCards = [
   {
     name: "Архыз",
@@ -101,64 +99,79 @@ const initialCards = [
     link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
   },
 ];
+//  СОЗДАНИЕ КАРТОЧКИ
 
-// Добавить начальные карточки
+function createCard(title, src) {
+  const card = cardTemplate.querySelector(".elements__element").cloneNode(true);
+  const cardImage = card.querySelector(".elements__image");
+  card.querySelector(".elements__title").textContent = title;
 
-function addCards(item) {
-  const placeElement = placeTemplate
-    .querySelector(".elements__template")
-    .cloneNode(true);
-  placeElement.querySelector(".elements__image").src = item.link;
-  placeElement.querySelector(".elements__title").textContent = item.name;
-  elements.append(placeElement);
+  cardImage.src = src;
+  cardImage.alt = title;
+  cardImage.addEventListener("click", () => openPopupImage(title, src));
+  card
+    .querySelector(".elements__trash")
+    .addEventListener("click", () => deleteCard(card));
+  card
+    .querySelector(".elements__icon")
+    .addEventListener("click", () => addOrRemoveLike(card));
+
+  console.log(card);
+  return card;
 }
-initialCards.forEach(addCards);
 
-// Добавить новую карточку
+//ДОБАВИТЬ 6 НАЧАЛЬНЫХ КАРТОЧЕК
+function addInitialCards() {
+  initialCards.forEach(function (item) {
+    src = item.link;
+    title = item.name;
+    renderCards(title, src, elements, "append");
+  });
+}
+addInitialCards();
 
+// ДОБАВИТЬ СОБСТВЕННУЮ КАРТОЧКУ
 function addPlaceCard(evt) {
   evt.preventDefault();
-  const placeInput = document.querySelector(".form__text_type_place").value;
-  const imageLinkImput = document.querySelector(".form__text_type_link").value;
-  const placeElement = placeTemplate
-    .querySelector(".elements__template")
-    .cloneNode(true);
-  initialCards.push({ name: placeInput, link: imageLinkImput });
-  item = initialCards[initialCards.length - 1];
-  placeElement.querySelector(".elements__image").src = item.link;
-  placeElement.querySelector(".elements__title").textContent = item.name;
-  elements.prepend(placeElement);
-  closePopup(popupPlace);
+  title = document.querySelector(".form__text_type_place").value;
+  src = document.querySelector(".form__text_type_link").value;
+
+  renderCards(title, src, elements);
+  closePopup(popupTypePlace);
 }
 formElementPlace.addEventListener("submit", addPlaceCard);
 
-// ПРОВЕРЯЕМ КУДА КЛИКНУЛИ
-elements.onclick = function (event) {
-  const target = event.target;
-  if (target.classList.contains("elements__icon")) {
-    addOrRemoveLike(target);
-  } else if (target.classList.contains("elements__image")) {
-    openPopupImage(target);
-  } else if (target.classList.contains("elements__trash")) {
-    deleteCard(target);
-  } else return;
-};
-
+// РЕНДЕР КАРТОЧЕК
+function renderCards(title, src, container, position = "prepend") {
+  const renderCard = createCard(title, src);
+  switch (position) {
+    case "append":
+      return container.append(renderCard);
+    case "prepend":
+      return container.prepend(renderCard);
+    default:
+      return;
+  }
+}
 // ПОСТАВИТЬ И УБРАТЬ ЛАЙК
 
-function addOrRemoveLike(btn) {
+function addOrRemoveLike(card) {
+  btn = card.querySelector(".elements__icon");
   btn.classList.toggle("elements__icon_type_active");
 }
+
 // УДАЛИТЬ КАРТОЧКУ
 
-function deleteCard(btn) {
-  btn.closest(".elements__template").remove();
+function deleteCard(card) {
+  btn = card.querySelector(".elements__trash");
+  btn.closest(".elements__element").remove();
 }
 
-// КОГДА НАЖАЛ НА КАРТИНКУ
+// Открыть попап места
 
-function openPopupImage(img) {
-  openPopup(popupImage);
-  popupImageSrc.src = img.src;
-  // popupImageText.textContent = imageTitle.textContent;
+function openPopupImage(title, src) {
+  popupImage.src = src;
+  popupImageText.textContent = title;
+  popupImage.alt = title;
+  openPopup(popupTypeImage);
 }
