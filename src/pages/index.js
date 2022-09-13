0f0cd208d90de4c1af2c253b78a54d709446be16
Gleb Lyashenkov <glepka17@yandex.ru>
2022-09-13 17:59:08 +0300

    Исправил ошибки первого ревью

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,77 +6,25 @@ import { PopupWithForm } from "../components/PopupWithForm.js";
 import { PopupWithImage } from "../components/PopupWithImage.js";
 import { Section } from "../components/Section.js";
 import { UserInfo } from "../components/UserInfo.js";
-
-// КАРТОЧКИ
-const initialCards = [
-  {
-    name: "Архыз",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
-  },
-  {
-    name: "Челябинская область",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
-  },
-  {
-    name: "Иваново",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
-  },
-  {
-    name: "Камчатка",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
-  },
-  {
-    name: "Холмогорский район",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
-  },
-  {
-    name: "Байкал",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
-  },
-];
-
-// КОНФИГ ФОРМЫ
-const validationConfig = {
-  formSelector: ".form",
-  inputSelector: ".form__text",
-  submitButtonSelector: ".form__submit-btn",
-  inactiveButtonClass: "form__submit-btn_type_disabled",
-  inputErrorClass: "form__input-error",
-  errorClass: "form__text_type_error",
-};
-
-// ПЕРЕМЕННЫЕ
-// ------------------------
-
-const elements = document.querySelector(".elements");
-const templateCardSelectorDefault = ".card-template_type_default";
-//ПОПАПЫ
-
-const popupTypeProfile = ".popup_type_profile";
-const popupTypePlace = ".popup_type_place";
-const popupTypeImage = ".popup_type_image";
-
-// ДЛЯ ОТКРЫТИЯ ПОПАПОВ
-const popupProfileOpenButton = document.querySelector(".profile__edit-button");
-const popupAddPlaceButton = document.querySelector(".profile__add-button");
-
-// ДАННЫЕ ПРОФИЛЯ, КОТОРЫЕ ВВОДИТ ПОЛЬЗОВАТЕЛЬ
-const nameInput = document.querySelector(".form__text_type_name");
-const professionInput = document.querySelector(".form__text_type_profession");
-
-// ДАННЫЕ ПРОФИЛЯ
-const profileName = ".profile__name";
-const profileAbout = ".profile__subtitle";
-const inputProfileName = "name";
-const inputProfileAboit = "profession";
-
-// ИНПУТЫ ПОПАПА ДОБАВЛЕНИЯ МЕСТА
-const placeInputText = ".form__text_type_place";
-const placeInputLink = ".form__text_type_link";
-
-// ФОРМЫ
-const profileForm = document.querySelector(".form-profile");
-const placeForm = document.querySelector(".form-place");
+import {
+  initialCards,
+  validationConfig,
+  cardsContainer,
+  templateCardDefaultSelector,
+  popupTypeProfileSelector,
+  popupTypePlaceSelector,
+  popupTypeImageSelector,
+  popupProfileOpenButton,
+  popupAddPlaceButton,
+  nameInput,
+  professionInput,
+  profileNameSelector,
+  profileAboutSelector,
+  inputProfileNameSelector,
+  inputProfileAboutSelector,
+  profileForm,
+  placeForm,
+} from "./utils/constants.js";
 
 // Классы
 // формы
@@ -87,34 +35,39 @@ const formValidPlace = new FormValidator(validationConfig, placeForm);
 formValidPlace.enableValidation();
 
 // Инфо профиля
-const userInfo = new UserInfo({ profileName, profileAbout });
+const userInfo = new UserInfo({ profileNameSelector, profileAboutSelector });
 
 // Попапы
-const popupWithImage = new PopupWithImage(popupTypeImage);
+const popupWithImage = new PopupWithImage(popupTypeImageSelector);
 popupWithImage.setEventListeners();
+
+// СОЗДАНИЕ КАРТОЧКИ
+const createCard = (item) => {
+  const newCard = new Card(item, templateCardDefaultSelector, () => {
+    popupWithImage.open(item.title, item.src);
+  });
+  return newCard.getCard();
+};
 
 const cardSection = new Section(
   {
     items: initialCards,
     renderer: (card) => {
-      const newCard = new Card(card, templateCardSelectorDefault, () => {
-        popupWithImage.open(card.name, card.link);
-      });
-      return newCard.createCard();
+      return createCard(card);
     },
   },
-  elements
+  cardsContainer
 );
 cardSection.renderItems();
 
 // ПОПАП ПРОФИЛЯ
 
 const popupUserInfo = new PopupWithForm(
-  popupTypeProfile,
+  popupTypeProfileSelector,
   (data) => {
     userInfo.setUserInfo({
-      userName: data[inputProfileName],
-      userAbout: data[inputProfileAboit],
+      userName: data[inputProfileNameSelector],
+      userAbout: data[inputProfileAboutSelector],
     });
   },
   () => {
@@ -131,17 +84,13 @@ popupProfileOpenButton.addEventListener("click", () => {
 
 // Добавить свою карточку
 const popupImg = new PopupWithForm(
-  popupTypePlace,
+  popupTypePlaceSelector,
   (data) => {
     const card = {
       name: data.place,
       link: data.link,
     };
-
-    const newCard = new Card(card, templateCardSelectorDefault, () => {
-      popupWithImage.open(card.name, card.link);
-    });
-    cardSection.addItem(newCard.createCard());
+    cardSection.addItem(createCard(card));
   },
   () => {
     formValidPlace.clearValidationErrors();
---
+++ src/utils/constants.js
@@ -0,0 +1,71 @@
+export const initialCards = [
+  {
+    name: "Архыз",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+  },
+  {
+    name: "Челябинская область",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+  },
+  {
+    name: "Иваново",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+  },
+  {
+    name: "Камчатка",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+  },
+  {
+    name: "Холмогорский район",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+  },
+  {
+    name: "Байкал",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
+];
+
+// КОНФИГ ФОРМЫ
+export const validationConfig = {
+  formSelector: ".form",
+  inputSelector: ".form__text",
+  submitButtonSelector: ".form__submit-btn",
+  inactiveButtonClass: "form__submit-btn_type_disabled",
+  inputErrorClass: "form__input-error",
+  errorClass: "form__text_type_error",
+};
+
+// ПЕРЕМЕННЫЕ
+// ------------------------
+
+export const cardsContainer = document.querySelector(".elements");
+export const templateCardDefaultSelector = ".card-template_type_default";
+//ПОПАПЫ
+
+export const popupTypeProfileSelector = ".popup_type_profile";
+export const popupTypePlaceSelector = ".popup_type_place";
+export const popupTypeImageSelector = ".popup_type_image";
+
+// ДЛЯ ОТКРЫТИЯ ПОПАПОВ
+export const popupProfileOpenButton = document.querySelector(
+  ".profile__edit-button"
+);
+export const popupAddPlaceButton = document.querySelector(
+  ".profile__add-button"
+);
+
+// ДАННЫЕ ПРОФИЛЯ, КОТОРЫЕ ВВОДИТ ПОЛЬЗОВАТЕЛЬ
+export const nameInput = document.querySelector(".form__text_type_name");
+export const professionInput = document.querySelector(
+  ".form__text_type_profession"
+);
+
+// ДАННЫЕ ПРОФИЛЯ
+export const profileNameSelector = ".profile__name";
+export const profileAboutSelector = ".profile__subtitle";
+export const inputProfileNameSelector = "name";
+export const inputProfileAboitSelector = "profession";
+
+// ФОРМЫ
+export const profileForm = document.querySelector(".form-profile");
+export const placeForm = document.querySelector(".form-place");
